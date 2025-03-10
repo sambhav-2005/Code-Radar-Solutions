5eb12e2d7610fabd8fd039e6c74761d21416e197
Sambhav Narula <narulasambhav24@gmail.com>
2025-03-10 14:25:11 +0530

    Solution to Find All unique Pairs with a Given Sum

--- Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
+++ Find All unique Pairs with a Given Sum@@676c02002dbc8c3e7a280b74/code.c
@@ -20,8 +20,8 @@ int pairSum(int arr[], int size, int targetSum, int arr2[][2])
                 arr2[size_arr2][0] = arr[i];
                 arr2[size_arr2][1] = arr[j];
                 size_arr2++;
-                break;
             }
+            break;
         }
     }
     return size_arr2;

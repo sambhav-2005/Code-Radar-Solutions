38eb258c584fb352461bc177cc1ad00b8584817e
Sambhav Narula <narulasambhav24@gmail.com>
2025-03-27 10:39:29 +0530

    Solution to Digit Sequence Mountain Peak Finder

---
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -0,0 +1,45 @@
+void swap(int a, int b)
+{
+    int temp;
+    int a = temp;
+    a = b;
+    b = temp;
+}
+
+int mountainPeak(int N)
+{
+    int arr[100];
+    int size_arr;
+    while(N > 0)
+    {
+        int digit = N % 10;
+        arr[size_arr] = digit;
+        size_arr++;
+        N /= 10;
+    }
+    int start = 0;
+    int end = size_arr - 1;
+
+    while(start <= end)
+    {
+        swap(arr[start], arr[end])
+        start++;
+        end--;
+    }
+
+    int mid = (start + end) / 2;
+    
+    while(start  < end)
+    {
+        if(arr[mid] < arr[mid + 1])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid;
+        }
+        mid = (start + end) / 2;
+    }
+    return arr[start];
+}
